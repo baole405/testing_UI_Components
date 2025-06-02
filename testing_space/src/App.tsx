fc105116148be9375836489cceddee3c15400b28
baole405 <baolttse184569@fpt.edu.vn>
2025-06-02 13:44:01 +0700

    add

--- testing_space/src/App.tsx
+++ testing_space/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import Homepage from './Homepage';
+// import Login from './Login';
+// import Register from './Register';
+// import Homepage from './Homepage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/homepage" element={<Homepage />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} /> */}
       </Routes>
     </Router>
   );
